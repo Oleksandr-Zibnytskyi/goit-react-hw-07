9f2d2164127d1dbfe079216af84daf88d31202f6
Oleksandr-Zibnytskyi <148706271+Oleksandr-Zibnytskyi@users.noreply.github.com>
2024-06-06 15:35:54 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import UserContacts from "./contacts.json";
+
+
+function App() {
+  const [filter, setFilter] = useState("");
+  const [contacts, setContacts] = useState(() => {
+    const storageContact = window.localStorage.getItem("saveContact");
+    return storageContact !== null ? JSON.parse(storageContact) : UserContacts;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("saveContact", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
+import Contact from '../Contact/Contact';
+
+import css from './ContactList.module.css'
+
+function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={css.list}>
+     {filteredContacts.map(({ id, name, number }) => (
+        <Contact key={id} id={id} name={name} number={number} />
+      ))}
+    </ul>
+  );
+}
+  
+  export default ContactList;
